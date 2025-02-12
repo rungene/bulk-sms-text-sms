d954c4a285652285d9e98b0d11cb156a3aa7a72d
rungene <rungenesamuel@gmail.com>
2025-02-12 09:47:03 +0000

    Reading csv int dictionary.Adding a check if 2 phone numbers appear on same cell

--- read_phone_numbers.py
+++ read_phone_numbers.py
@@ -2,7 +2,7 @@
 import csv
 
 
-def read_kenyan_phone_numbers(filename="sample.csv", delimiter=","):
+def read_kenyan_phone_numbers(filename="sample.csv"):
     """Reads phone numbers from a CSV file, replaces numbers
     starting with '+254' with '0',and returns a list of
     modified phone numbers.
@@ -10,8 +10,6 @@ def read_kenyan_phone_numbers(filename="sample.csv", delimiter=","):
     Args:
         filename (str, optional): The name of the CSV file to read.
         Defaults to "sample.csv".
-        delimiter (str, optional): The delimiter used in the CSV
-        file. Defaults to "," (comma).
 
     Returns:
         list: A list of modified phone numbers (strings).
@@ -19,16 +17,13 @@ def read_kenyan_phone_numbers(filename="sample.csv", delimiter=","):
 
     kenyan_phone_numbers = []
     try:
-        with open(filename) as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=delimiter)
+        with open(filename, mode='r') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
             line_count = 0
             for row in csv_reader:
-                if line_count == 0:
-                    # Skip header - first row contains column names
-                    line_count += 1
-                    continue
-
-                phone = row[35].strip()
+                phone = row['Phone 1 - Value']
+                if ':::' in phone:
+                    phone = phone.split(':::')[0]
                 phone_number = phone.replace(' ', '')
                 if phone_number.startswith('0') or \
                         phone_number.startswith('+254') or \
@@ -38,9 +33,6 @@ def read_kenyan_phone_numbers(filename="sample.csv", delimiter=","):
                     elif phone_number.startswith('254'):
                         phone_number = '0' + phone_number[3:]
                     kenyan_phone_numbers.append(phone_number)
-                line_count += 1
-                if line_count == 2:
-                    break
 
     except FileNotFoundError:
         print(f'Error: file "{filename}" not found')
@@ -51,5 +43,5 @@ def read_kenyan_phone_numbers(filename="sample.csv", delimiter=","):
 
 
 if __name__ == "__main__":
-    modified_numbers = read_kenyan_phone_numbers(filename="contacts.csv")
+    modified_numbers = read_kenyan_phone_numbers(filename="contacts_try.csv")
     print(modified_numbers)
